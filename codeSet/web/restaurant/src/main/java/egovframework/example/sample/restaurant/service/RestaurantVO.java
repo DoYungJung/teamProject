b89228804f3614ce4e7a94b54dfaa335f31f61aa
jinhwan4511 <raul.lee.4511@gmail.com>
2022-07-03 16:32:50 +0900

    웹 프로젝트업로드

---
+++ codeSet/web/restaurant/src/main/java/egovframework/example/sample/restaurant/service/RestaurantVO.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package egovframework.example.sample.restaurant.service;
+
+import egovframework.example.sample.service.SampleDefaultVO;
+
+/**
+ * @Class Name : SampleVO.java
+ * @Description : SampleVO Class
+ * @Modification Information
+ * @
+ * @  수정일      수정자              수정내용
+ * @ ---------   ---------   -------------------------------
+ * @ 2009.03.16           최초생성
+ *
+ * @author 개발프레임웍크 실행환경 개발팀
+ * @since 2009. 03.16
+ * @version 1.0
+ * @see
+ *
+ *  Copyright (C) by MOPAS All right reserved.
+ */
+public class RestaurantVO extends SampleDefaultVO {
+
+	//private static final long serialVersionUID = 1L;
+	
+	private String reviewNo;
+	private String restaurantNo;
+	private String title;  
+	private String content;
+	private String writerNm;
+	private String writerId;
+	private String regDate;
+	private String modDate;
+	private String count;
+	
+	private String userId;
+	private String userPw;
+	private String userNm;
+	private String useYn;
+	
+	
+	public String getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(String reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public String getRestaurantNo() {
+		return restaurantNo;
+	}
+	public void setRestaurantNo(String restaurantNo) {
+		this.restaurantNo = restaurantNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriterNm() {
+		return writerNm;
+	}
+	public void setWriterNm(String writerNm) {
+		this.writerNm = writerNm;
+	}
+	public String getWriterId() {
+		return writerId;
+	}
+	public void setWriterId(String writerId) {
+		this.writerId = writerId;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public String getModDate() {
+		return modDate;
+	}
+	public void setModDate(String modDate) {
+		this.modDate = modDate;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserNm() {
+		return userNm;
+	}
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+	public String getUseYn() {
+		return useYn;
+	}
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+	
+	
+	
+
+	
+	
+
+}

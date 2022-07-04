a9e86fd4297978216ec1d194dd072f8740dbfae0
leegisul4242 <leegisul@naver.com>
2022-07-04 17:38:55 +0900

    컨트롤러 수정 테스트

--- codeSet/web/restaurant/restaurant/src/main/java/egovframework/example/sample/restaurant/web/RestaurantController.java
+++ codeSet/web/restaurant/restaurant/src/main/java/egovframework/example/sample/restaurant/web/RestaurantController.java
@@ -1,12 +1,18 @@
 package egovframework.example.sample.restaurant.web;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import egovframework.example.sample.restaurant.service.RestaurantService;
+import egovframework.example.sample.restaurant.service.RestaurantVO;
 
 @Controller
 public class RestaurantController {
@@ -24,6 +30,19 @@ public class RestaurantController {
 	@RequestMapping(value = "/testLayout.do")
 	public String test1(ModelMap model) throws Exception {
 		return "/restaurant/testLayout";
+	}
+	
+	@RequestMapping(value = "/mgmt.do")
+	public String reviewList(ModelMap model,  @ModelAttribute("restaurantVO") RestaurantVO restaurantVO, HttpServletRequest request) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		Calendar c1 = Calendar.getInstance();
+		String strToday = sdf.format(c1.getTime());
+		
+//		restaurantVO.setRegDate(regDate);(strToday);
+//		restaurantVO.setWriterId(writerId);(request.getSession().getAttribute("userId").toString()); //댓글등록할 유저네임 자동뿌려줘야해서
+//		restaurantVO.setWriterNm(request.getSession().getAttribute("userName").toString());
+		return "/restaurant/mgmt";
 	}
 
 }
